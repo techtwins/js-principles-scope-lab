7b80e0d75ff752c2866f387b2c8cc2c4d9f1d4a2
techtwins <gshevaughn@gmail.com>
2020-11-15 12:41:38 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'value'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'
+}
